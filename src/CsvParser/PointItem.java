f5c2f963d4c102c62b028d0bb4133155fec5d284
Mojdeh Soltan <mohamadi.mojde@yahoo.com>
2014-04-11 11:16:16 +0200

    pointItem of package CsvParser

---
+++ src/CsvParser/PointItem.java
@@ -0,0 +1,18 @@
+package CsvParser;
+
+public class PointItem {
+	public String pointId;
+	public String pointLatitude;
+	public String pointLongitude;
+	public String pointAltitude;
+	public String pointDirection;
+	public String pointSpeed;
+	public String pointTime;
+	
+	@Override
+	public String toString() {
+		return "ID:" + pointId + ", Latitude:" + pointLatitude + ", Longitude:"
+				+ pointLongitude + ", Altitude:" + pointAltitude + ", Direction:"
+				+ pointDirection + ", Speed:" + pointSpeed + ", Time:" + pointTime;
+	}
+}
